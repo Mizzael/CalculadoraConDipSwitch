e26fa0925e487e9a073579c2940699159ee96c60
Missael <jonathanmissael_jimenez@ucol.mx>
2019-06-21 11:46:46 -0500

    Probando la primea sentencia

--- CalculadoraSwitch.c
+++ CalculadoraSwitch.c
@@ -1,6 +1,6 @@
 #include <18F4620.h>
-#fuses HS,NOWDT,NOMCLR
-#use delay(clock=40 MHZ)
+#fuses HS,NOWDT,NOMCLR, /*INTRC_IO*/
+#use delay(clock=16 MHZ)
 
 //Aqui deben ir las interfaces tales como la rs-232, I^2C
 /*
@@ -23,10 +23,27 @@
 //aqui el void main(void)
 void main(void)
 {
+   int Valor1;
+   int Valor2;
+   int Result;
+   int Operacion;
+   
+   set_tris_a(0xC0);
+   set_tris_b(0x80);
+   set_tris_c(0xFF);
+   set_tris_d(0xFF);
+   set_tris_e(0x07);
    //Aqui iniciaizamos todo
    while(1)
    {
-            
+      Valor1=input_c();
+      Valor2=input_d();
+      
+      if((Operacion&0x80)==0x80){
+         Result=Valor1+Valor2;
+         Operacion=0x00;
+      }
+      output_a(Result);
    }
 }
 
